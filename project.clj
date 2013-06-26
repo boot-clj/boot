06ce72606fef0faf2afe4b2ac198b36e799c58c9
Alan Dipert <alan@dipert.org>
2013-06-26 14:07:14 -0400

    bring in maven-model

--- project.clj
+++ project.clj
@@ -5,6 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.cemerick/pomegranate "0.2.0" :exclusions [org.clojure/clojure]]
+                 ; for pom middleware
+                 [org.apache.maven/maven-model "3.0.4"
+                  :exclusions
+                  [org.codehaus.plexus/plexus-utils]]
                  ; for cljsbuild middleware 
                  [org.clojure/clojurescript "0.0-1820"]
                  ; for mtime middleware
