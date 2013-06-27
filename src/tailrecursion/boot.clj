363cc896133305cf3b70cc49c2c6892526633364
Micha Niskin <asdf@ubergibson.com>
2013-06-27 18:57:24 -0400

    Fix bugs with deps installation and remove coords arg from deps fn

--- src/tailrecursion/boot.clj
+++ src/tailrecursion/boot.clj
@@ -10,17 +10,16 @@
 (def base-env
   {:boot {:dependencies #{}
           :directories #{}
-          :repositories #{}
+          :repositories {}
           :system {:jvm-opts (vec (.. ManagementFactory getRuntimeMXBean getInputArguments))
                    :bootfile (io/file (System/getProperty "user.dir") "boot.clj")
                    :cwd      (io/file (System/getProperty "user.dir"))}
-          :pomegranate {:installed #{}}}})
+          :installed #{}}})
 
 (def env (atom base-env))
 
 (def ^:dynamic *default-repositories*
-  #{"http://repo1.maven.org/maven2/"
-    "http://clojars.org/repo/"})
+  {"http://repo1.maven.org/maven2/" "http://clojars.org/repo/"})
 
 (defn find-idx [v val]
   (ffirst (filter (comp #{val} second) (map vector (range) v))))
@@ -32,15 +31,11 @@
     (into coordinate [:exclusions syms])))
 
 (defn install [{:keys [coordinates repositories]}]
-  (let [deps (mapv (partial exclude ['org.clojure/clojure]) coordinates)
-        repos (into *default-repositories* repositories)
-        installed (pom/add-dependencies
-                   :coordinates deps
-                   :repositories (apply hash-map (mapcat (partial repeat 2) repos)))]
-    (swap! env (partial merge-with into)
-           {:boot {:dependencies deps
-                   :repositories repos
-                   :pomegranate {:installed installed}}})))
+  (let [deps      (mapv (partial exclude ['org.clojure/clojure]) coordinates)
+        repos     (into *default-repositories* repositories)
+        installed (pom/add-dependencies :coordinates deps :repositories repos)
+        new-deps  {:dependencies deps :repositories repos :installed installed}]
+    (swap! env #(update-in %1 [:boot] (partial merge-with into) new-deps))))
 
 (defn add [dirs]
   (when (seq dirs)
--- src/tailrecursion/boot/deps.clj
+++ src/tailrecursion/boot/deps.clj
@@ -3,19 +3,21 @@
     [java.util.jar JarFile]
     [java.util.zip ZipFile])
   (:require
-    [cemerick.pomegranate.aether :refer [resolve-dependencies]]
-    [tailrecursion.boot.kahnsort :refer [topo-sort]]))
+    [tailrecursion.boot          :as boot]
+    [tailrecursion.boot.kahnsort :refer [topo-sort]]
+    [cemerick.pomegranate.aether :refer [resolve-dependencies]]))
 
-(defn entries-1 [jar]
+(defn entries [jar]
   (->> (.entries jar)
     enumeration-seq
     (map #(vector (.getName %) (.getInputStream jar %)))
     (into {})))
 
-(defn deps [coords]
-  (->> (resolve-dependencies :coordinates coords)
-    (topo-sort)
-    (map #(:file (meta %)))
-    (filter #(.endsWith (.getPath %) ".jar"))
-    (map #(JarFile. %))
-    (map #(vector (.getName %) (entries-1 %)))))
+(defn deps []
+  (let [{repos :repositories coords :dependencies} (:boot @boot/env)]
+    (->> (resolve-dependencies :repositories repos :coordinates coords)
+      (topo-sort)
+      (map #(:file (meta %)))
+      (filter #(.endsWith (.getPath %) ".jar"))
+      (map #(JarFile. %))
+      (map #(vector (.getName %) (entries %))))))
