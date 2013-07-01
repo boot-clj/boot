750573dca18b67951259e46e9e3f2e91efd4f23f
Micha Niskin <asdf@ubergibson.com>
2013-07-01 13:40:31 -0400

    Refactor to remove coupling between cljsbuild and hoplon

--- src/tailrecursion/boot/middleware/cljsbuild.clj
+++ src/tailrecursion/boot/middleware/cljsbuild.clj
@@ -1,7 +1,9 @@
 (ns tailrecursion.boot.middleware.cljsbuild
   (:require
     [cljs.closure :as cljs]
-    [clojure.java.io                :refer [file]]
+    [clojure.string                 :refer [split join]]
+    [clojure.java.io                :refer [file make-parents]]
+    [tailrecursion.boot.deps        :refer [deps]]  
     [tailrecursion.boot.tmpregistry :refer [mk mkdir exists? unmk]]))
 
 (defrecord SourcePaths [paths]
@@ -20,19 +22,51 @@
    :foreign-libs  []
    :pretty-print  true})
 
+(let [last-counter (atom 0)]
+  (def counter #(swap! last-counter inc)))
+
+(defn install-deps [jars incs exts libs flibs]
+  (let [name*   #(.getName (file %))
+        match   #(last (re-find #"[^.]+\.([^.]+)\.js$" %))
+        dirmap  {"inc" incs "ext" exts "lib" libs "flib" flibs}
+        outfile #(file %1 (str (format "%010d" (counter)) "_" (name* %2)))
+        write   #(if-let [d (dirmap (match %1))]
+                   (spit (doto (outfile d %1) make-parents) (slurp %2)))]
+    (doall (->> jars (map second) (mapcat identity) reverse (map (partial apply write))))))
+
+(defn update-by-keys [m ks f] (reduce #(assoc %1 %2 (f (%1 %2))) m ks))
+
 (defn cljsbuild [handler]
   (fn [spec]
-    (let [cspec (merge-with
-                  (comp (partial some identity) vector)
-                  (-> dfl-opts
-                    (merge (:cljsbuild spec))
-                    (update-in [:output-to] file)
-                    (update-in [:output-dir] file)) 
-                  {:output-to   (mk ::js "main.js")
-                   :output-dir  (mkdir ::out)})
+    (let [{:keys [output-to flib-out lib-out ext-out inc-out] :as cspec}
+          (merge-with
+            (comp (partial some identity) vector)
+            (-> dfl-opts
+              (merge (:cljsbuild spec))
+              (update-by-keys
+                [:output-to :output-dir :flib-out :lib-out :ext-out :inc-out]
+                file))
+            {:output-to   (mk ::js "main.js")
+             :output-dir  (mkdir ::out)
+             :flib-out    (mkdir ::flib)
+             :lib-out     (mkdir ::lib)
+             :ext-out     (mkdir ::ext)
+             :inc-out     (mkdir ::inc)})
+          file? #(.isFile %)
+          path* #(.getPath %)
+          files #(filter file? (file-seq %))
+          paths #(mapv path* (files %))
+          cat   #(join "\n" (mapv slurp %)) 
           srcs  (SourcePaths. (:source-paths cspec)) 
+          _     (install-deps (deps) inc-out ext-out lib-out flib-out) 
+          exts  (paths ext-out)
+          incs  (cat (sort (files inc-out)))
           opts  (-> cspec
+                  (select-keys [:output-to    :output-dir   :optimizations
+                                :warnings     :externs      :libs
+                                :foreign-libs :pretty-print])
                   (update-in [:output-to] #(.getPath %))
-                  (dissoc :source-paths))]
+                  (update-in [:externs] into exts))]
       (cljs/build srcs opts)
+      (spit output-to (str incs "\n" (slurp output-to)))
       (handler (assoc spec :cljsbuild cspec)))))
--- src/tailrecursion/boot/middleware/hoplon.clj
+++ src/tailrecursion/boot/middleware/hoplon.clj
@@ -1,42 +1,26 @@
 (ns tailrecursion.boot.middleware.hoplon
   (:require
     [clojure.java.io                          :refer [file make-parents]]
-    [tailrecursion.boot.deps                  :refer [deps]]
     [tailrecursion.boot.tmpregistry           :refer [mk mkdir exists? unmk]]
-    [tailrecursion.hoplon.compiler.compiler   :refer [install-deps compile-dir]]))
+    [tailrecursion.hoplon.compiler.compiler   :refer [compile-dir]]))
 
-(def dfl-opts     {:source-dir "src/html"})
-(def update-vals  #(into {} (map (fn [[k v]] [k (apply %2 v %&)]) %1)))
+(def dfl-opts 
+  {:source-dir  "src/html"
+   :html-out    nil
+   :cljs-out    nil})
+
+(defn update-by-keys [m ks f] (reduce #(assoc %1 %2 (f (%1 %2))) m ks))
 
 (defn hoplon [handler]
   (fn [spec]
-    (let [{:keys [source-dir  html-out  cljs-out
-                  flib-out    lib-out   ext-out
-                  inc-out] :as hspec}
+    {:pre [(get-in spec [:hoplon :compiled-js])]}
+    (let [{:keys [source-dir html-out cljs-out compiled-js] :as hspec}
           (merge-with 
             (comp (partial some identity) vector)
-            (-> dfl-opts (merge (:hoplon spec)) (update-vals file)) 
-            {:html-out  (mkdir ::html)
-             :cljs-out  (mkdir ::cljs)
-             :flib-out  (mkdir ::flib)
-             :lib-out   (mkdir ::lib)
-             :ext-out   (mkdir ::ext)
-             :inc-out   (mkdir ::inc)})
-          file?   #(.isFile %)
-          name*   #(.getName %)
-          path*   #(.getPath %)
-          js-out  (doto (file html-out "main.js") make-parents)
-          files   #(->> (file-seq %1) (filter file?))
-          cat-str #(->> (map slurp %) (interpose "\n") (apply str))
-          inc-str #(cat-str (sort (map path* (files inc-out))))
-          cljsopt #(->>
-                     {:source-paths  [(.getPath cljs-out)]
-                      :externs       (mapv path* (files ext-out))
-                      :libs          (mapv path* (files lib-out))}
-                     (update-in spec [:cljsbuild] (partial merge-with into)))]
-      (install-deps (deps) inc-out ext-out lib-out flib-out) 
-      (compile-dir source-dir cljs-out html-out)
-      (let [rspec (handler (assoc (cljsopt) :hoplon hspec))
-            compiled-js (slurp (get-in rspec [:cljsbuild :output-to]))]
-        (spit js-out (str (inc-str) "\n" compiled-js))
-        rspec))))
+            (-> dfl-opts
+              (merge (:hoplon spec))
+              (update-by-keys [:source-dir :html-out :cljs-out] file)) 
+            {:html-out (mkdir ::html)
+             :cljs-out (mkdir ::cljs)})]
+      (compile-dir compiled-js source-dir cljs-out html-out)
+      (handler (assoc spec :hoplon hspec)))))
