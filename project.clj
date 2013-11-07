50851c16645e58c4e9723cd5010b8361bb33f185
Micha Niskin <asdf@ubergibson.com>
2013-11-07 18:30:10 -0500

    cleanup

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject tailrecursion/boot "0.2.1"
-  :description "A dependency setup/build tool for Clojure."
-  :url "https://github.com/tailrecursion/boot"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.cemerick/pomegranate "0.2.0" :exclusions [org.clojure/clojure]]
-                 [digest "1.4.3" :exclusions [org.clojure/clojure]]]
-  :main tailrecursion.boot)
+  :description  "A dependency setup/build tool for Clojure."
+  :url          "https://github.com/tailrecursion/boot"
+  :license      {:name  "Eclipse Public License"
+                 :url   "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure       "1.5.1"]
+                 [com.cemerick/pomegranate  "0.2.0" :exclusions [org.clojure/clojure]]
+                 [digest                    "1.4.3" :exclusions [org.clojure/clojure]]]
+  :aot          :all
+  :main         tailrecursion.boot)
