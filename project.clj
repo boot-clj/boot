4ff357a3fe7f932f92dd4bfab778e550866bc367
Micha Niskin <micha.niskin@gmail.com>
2014-08-22 16:54:24 -0400

    1.1.0 -- update to use clojure 1.6.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject tailrecursion/boot "1.0.5"
+(defproject tailrecursion/boot "1.1.0"
   :description  "A dependency setup/build tool for Clojure."
   :url          "https://github.com/tailrecursion/boot"
   :license      {:name  "Eclipse Public License"
                  :url   "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :aot          :all
   :main         tailrecursion.boot.loader)
