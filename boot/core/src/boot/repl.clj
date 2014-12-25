f5e077770152012b30b289645458e8bff5c1db74
Micha Niskin <micha.niskin@gmail.com>
2014-12-24 19:18:13 -0500

    Don't pull in Clojure 1.2 with tools.nrepl anymore

--- boot/core/src/boot/repl.clj
+++ boot/core/src/boot/repl.clj
@@ -1,7 +1,7 @@
 (ns boot.repl)
 
 (def ^:dynamic *default-dependencies*
-  (atom '[[org.clojure/tools.nrepl "0.2.6"]]))
+  (atom '[[org.clojure/tools.nrepl "0.2.6" :exclusions [[org.clojure/clojure]]]]))
 
 (def ^:dynamic *default-middleware*
   (atom ['boot.from.io.aviso.nrepl/pretty-middleware]))
