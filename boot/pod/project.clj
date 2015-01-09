0234e8441a54ee36781563693ba5114f1dabb0b3
Micha Niskin <micha.niskin@gmail.com>
2015-01-09 12:02:45 -0500

    Update shimdandy deps (thanks @tobias)

--- boot/pod/project.clj
+++ boot/pod/project.clj
@@ -9,10 +9,12 @@
   :description  "Boot pod module–this is included with all pods."
   :url          "http://github.com/boot-clj/boot"
   :scm          {:url "https://github.com/boot-clj/boot.git" :dir "../../"}
-  :repositories [["clojars"  {:url "https://clojars.org/repo" :creds :gpg :sign-releases false}]]
+  :repositories [["clojars"        {:url "https://clojars.org/repo" :creds :gpg :sign-releases false}]
+                 ["sonatype"       {:url "http://oss.sonatype.org/content/repositories/releases"}]
+                 ["sonatype-snaps" {:url "http://oss.sonatype.org/content/repositories/snapshots"}]]
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[boot/base                               ~version :scope "provided"]
                  [org.clojure/clojure                     "1.6.0"  :scope "provided"]
                  [org.tcrawley/dynapath                   "0.2.3"  :scope "compile"]
-                 [org.projectodd.shimdandy/shimdandy-impl "1.0.1"  :scope "compile"]])
+                 [org.projectodd.shimdandy/shimdandy-impl "1.1.0"  :scope "compile"]])
