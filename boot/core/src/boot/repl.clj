8b70067a87a54fd3e76fd17e2a5ed65119772bf8
Micha Niskin <micha.niskin@gmail.com>
2015-02-11 09:53:47 -0500

    Update nrepl dep (fixes #124)

--- boot/core/src/boot/repl.clj
+++ boot/core/src/boot/repl.clj
@@ -2,7 +2,7 @@
   (:require [boot.util :as util]))
 
 (def ^:dynamic *default-dependencies*
-  (atom '[[org.clojure/tools.nrepl "0.2.6" :exclusions [[org.clojure/clojure]]]]))
+  (atom '[[org.clojure/tools.nrepl "0.2.7" :exclusions [[org.clojure/clojure]]]]))
 
 (def ^:dynamic *default-middleware*
   (atom (if-not @util/*colorize?* [] ['boot.from.io.aviso.nrepl/pretty-middleware])))
