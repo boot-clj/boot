4e97d7c36ff1913e17c07132de4d9e3b61e16eb7
Micha Niskin <micha.niskin@gmail.com>
2015-09-02 15:31:01 -0400

    Fix #284 -- BOOT_JVM_OPTIONS: preserve quotes etc

--- head.sh
+++ head.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+declare -a "options=($BOOT_JVM_OPTIONS)"
 self="${BASH_SOURCE[0]}"
 selfdir="$(cd "$(dirname "${self}")" ; pwd)"
 selfpath="$selfdir/$(basename "$self")"
-exec ${BOOT_JAVA_COMMAND:-java} $BOOT_JVM_OPTIONS -Dboot.app.path="$selfpath" -jar "$0" "$@"
+exec ${BOOT_JAVA_COMMAND:-java} "${options[@]}" -Dboot.app.path="$selfpath" -jar "$0" "$@"
