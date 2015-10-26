6196ee164e866ca7d43ba5c44c617fb09969efbc
Martin Klepsch <martinklepsch@googlemail.com>
2015-10-26 21:34:17 +0100

    update clojure dep in pod dependencies

--- boot/pod/project.clj
+++ boot/pod/project.clj
@@ -16,6 +16,6 @@
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[boot/base                               ~version :scope "provided"]
-                 [org.clojure/clojure                     "1.6.0"  :scope "provided"]
+                 [org.clojure/clojure                     "1.7.0"  :scope "provided"]
                  [org.tcrawley/dynapath                   "0.2.3"  :scope "compile"]
                  [org.projectodd.shimdandy/shimdandy-impl "1.2.0"  :scope "compile"]])
