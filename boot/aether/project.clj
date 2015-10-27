73ce5a725aa76b2160cd898f1f69bece377bd97e
Martin Klepsch <martinklepsch@googlemail.com>
2015-10-27 14:45:06 +0100

    clojure versions must be in sync!

--- boot/aether/project.clj
+++ boot/aether/project.clj
@@ -13,6 +13,6 @@
   :repositories [["clojars" {:url "https://clojars.org/repo" :creds :gpg :sign-releases false}]]
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure      "1.7.0"  :scope "compile"]
+  :dependencies [[org.clojure/clojure      "1.6.0"  :scope "compile"]
                  [boot/pod                 ~version :scope "compile"]
                  [com.cemerick/pomegranate "0.3.0"  :scope "compile"]])
