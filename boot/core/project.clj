9e1e9d74d59f842241f9bf7554e8d9bbb8924148
Micha Niskin <micha.niskin@gmail.com>
2015-11-16 03:34:41 -0500

    move repl machinery to where it's available in any pod

--- boot/core/project.clj
+++ boot/core/project.clj
@@ -5,15 +5,15 @@
                (.getProperty "version")))
 
 (defproject boot/core version
-  :aot          [#"^(?!boot\.repl-server).*$"]
+  :aot            :all
   :jar-exclusions [#"^clojure/core/"]
-  :description  "Core boot module–boot scripts run in this pod."
-  :url          "http://github.com/boot-clj/boot"
-  :scm          {:url "https://github.com/boot-clj/boot.git" :dir "../../"}
-  :repositories [["clojars"  {:url "https://clojars.org/repo" :creds :gpg :sign-releases false}]]
-  :license      {:name "Eclipse Public License"
-                 :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"  :scope "provided"]
-                 [boot/base           ~version :scope "provided"]
-                 [boot/pod            ~version :scope "compile"]])
+  :description    "Core boot module–boot scripts run in this pod."
+  :url            "http://github.com/boot-clj/boot"
+  :scm            {:url "https://github.com/boot-clj/boot.git" :dir "../../"}
+  :repositories   [["clojars"  {:url "https://clojars.org/repo" :creds :gpg :sign-releases false}]]
+  :license        {:name "Eclipse Public License"
+                   :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies   [[org.clojure/clojure "1.6.0"  :scope "provided"]
+                   [boot/base           ~version :scope "provided"]
+                   [boot/pod            ~version :scope "compile"]])
 
--- boot/pod/project.clj
+++ boot/pod/project.clj
@@ -5,17 +5,17 @@
                (.getProperty "version")))
 
 (defproject boot/pod version
-  :aot :all
+  :aot            [#"^(?!boot\.repl-server).*$"]
   :jar-exclusions [#"^clojure/core/"]
-  :description  "Boot pod module–this is included with all pods."
-  :url          "http://github.com/boot-clj/boot"
-  :scm          {:url "https://github.com/boot-clj/boot.git" :dir "../../"}
-  :repositories [["clojars"        {:url "https://clojars.org/repo" :creds :gpg :sign-releases false}]
-                 ["sonatype"       {:url "http://oss.sonatype.org/content/repositories/releases"}]
-                 ["sonatype-snaps" {:url "http://oss.sonatype.org/content/repositories/snapshots"}]]
-  :license      {:name "Eclipse Public License"
-                 :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[boot/base                               ~version :scope "provided"]
-                 [org.clojure/clojure                     "1.6.0"  :scope "provided"]
-                 [org.tcrawley/dynapath                   "0.2.3"  :scope "compile"]
-                 [org.projectodd.shimdandy/shimdandy-impl "1.2.0"  :scope "compile"]])
+  :description    "Boot pod module–this is included with all pods."
+  :url            "http://github.com/boot-clj/boot"
+  :scm            {:url "https://github.com/boot-clj/boot.git" :dir "../../"}
+  :repositories   [["clojars"        {:url "https://clojars.org/repo" :creds :gpg :sign-releases false}]
+                   ["sonatype"       {:url "http://oss.sonatype.org/content/repositories/releases"}]
+                   ["sonatype-snaps" {:url "http://oss.sonatype.org/content/repositories/snapshots"}]]
+  :license        {:name "Eclipse Public License"
+                   :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies   [[boot/base                               ~version :scope "provided"]
+                   [org.clojure/clojure                     "1.6.0"  :scope "provided"]
+                   [org.tcrawley/dynapath                   "0.2.3"  :scope "compile"]
+                   [org.projectodd.shimdandy/shimdandy-impl "1.2.0"  :scope "compile"]])
