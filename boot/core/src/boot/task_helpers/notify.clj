50277bc5a96f0ec524b814e5c6cd6eedfdfa03c1
Geoff Shannon <geoffpshannon@gmail.com>
2016-02-04 21:51:17 -0600

    Alias clojure.java.shell instead of refer

--- boot/core/src/boot/task_helpers/notify.clj
+++ boot/core/src/boot/task_helpers/notify.clj
@@ -1,9 +1,9 @@
 (ns boot.task-helpers.notify
-  (:require [clojure.java.shell :refer [sh]]))
+  (:require [clojure.java.shell :as shell]))
 
 (defn- ^{:boot/from :jeluard/boot-notify} program-exists?
   [s]
-  (= 0 (:exit (sh "sh" "-c" (format "command -v %s" s)))))
+  (= 0 (:exit (shell/sh "sh" "-c" (format "command -v %s" s)))))
 
 (defmulti notify-method
   (fn [os _message]
@@ -12,13 +12,13 @@
 (defmethod notify-method "Mac OS X"
   [_ {:keys [message title icon pid] :as notification}]
   (if (program-exists? "terminal_notifier")
-    (sh "terminal-notifier" "-message" message "-title" title "-contentImage" icon "-group" pid)
+    (shell/sh "terminal-notifier" "-message" message "-title" title "-contentImage" icon "-group" pid)
     ((get-method notify-method :default) :default notification)))
 
 (defmethod notify-method "Linux"
   [_ {:keys [message title icon] :as notification}]
   (if (program-exists? "notify-send")
-    (sh "notify-send" title message "--icon" icon)
+    (shell/sh "notify-send" title message "--icon" icon)
     ((get-method notify-method :default) :default notification)))
 
 (defmethod notify-method :default
