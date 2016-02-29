9647362e76fa6e7dee13d670bf37e0fc1c16c73e
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-02-28 19:57:55 -0800

    Add tests and adjust existing in order to conform to deftesttask

--- boot/core/build.boot
+++ boot/core/build.boot
@@ -2,25 +2,15 @@
  :source-paths #{"test"}
  :dependencies '[[org.clojure/tools.reader "1.0.0-alpha2"]])
 
-(require '[boot.task.built-in-test :refer :all])
+(require '[boot.task.built-in-test]
+         '[boot.test-test])
+
 (import boot.App)
 
 (ns-unmap 'boot.user 'test)
 
-;; or from cmd line
-;; boot runtests -c "test1 --flag"
-;;               -c "test2"
-;;               -c ...
-
 (deftask test []
   (boot.util/info "Testing against version %s\n" (boot.App/config "BOOT_VERSION"))
-  (comp (runtests :commands #{"sift-with-meta-tests"
-                              "sift-with-meta-invert-tests"
-                              "sift-include-tests"
-                              "sift-include-invert-tests"
-                              "sift-to-asset-tests"
-                              "sift-to-asset-invert-tests"
-                              "sift-add-meta-tests"
-                              "sift-add-meta-invert-tests"})
+  (comp (runtests)
         (test-report)
         (test-exit)))
--- boot/core/test/boot/task/built_in_test.clj
+++ boot/core/test/boot/task/built_in_test.clj
@@ -1,8 +1,9 @@
 (ns boot.task.built-in-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.test :refer :all]
             [boot.core :refer :all]
             [boot.task.built-in :refer :all]
-            [boot.test :as boot-test]
+            [boot.test :as boot-test :refer [deftesttask]]
             [boot.pod :as pod]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -10,41 +11,39 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (deftask ^:private with-meta-tests []
-  (with-pre-wrap fileset
+  (with-pass-thru fileset
     (let [tmpfiles (output-files fileset)
           tmpfiles-with-meta (filter :boot-test-tag tmpfiles)
           tmpfiles-clj (by-re [#".clj$"] tmpfiles)]
       (is (= tmpfiles-with-meta tmpfiles-clj) "only .clj files should result from sift :with-meta #{boot-test-tag}"))
     fileset))
 
-(deftask sift-with-meta-tests []
-  (boot-test/test-task
-   (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
-         (sift :add-meta {#".clj$" :boot-test-tag})
-         (sift :with-meta #{:boot-test-tag})
-         (with-meta-tests))))
+(deftesttask sift-with-meta-tests []
+  (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
+        (sift :add-meta {#".clj$" :boot-test-tag})
+        (sift :with-meta #{:boot-test-tag})
+        (with-meta-tests)))
 
 (deftask ^:private with-meta-invert-tests []
-  (with-pre-wrap fileset
+  (with-pass-thru fileset
     (let [tmpfiles (output-files fileset)
           tmpfiles-without-meta (remove :boot-test-tag tmpfiles)
-          tmpfiles-not-clj (not-by-re [#".clj$"] tmpfiles)]
-      (is (= tmpfiles-without-meta tmpfiles-not-clj) "only non .clj files should result from sift :with-meta #{boot-test-tag} invert"))
+          tmpfiles-clj (by-re [#".clj$"] tmpfiles)]
+      (is (not (= tmpfiles-without-meta tmpfiles-clj)) "only non .clj files should result from sift :with-meta #{boot-test-tag} invert"))
     fileset))
 
-(deftask sift-with-meta-invert-tests []
-  (boot-test/test-task
-   (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
-         (sift :add-meta {#".clj$" :boot-test-tag})
-         (sift :with-meta #{:boot-test-tag} :invert true)
-         (with-meta-invert-tests))))
+(deftesttask sift-with-meta-invert-tests []
+  (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
+        (sift :add-meta {#".clj$" :boot-test-tag})
+        (sift :with-meta #{:boot-test-tag} :invert true)
+        (with-meta-invert-tests)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;;; sift --include  ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;
 
 (deftask ^:private  include-tests []
-  (with-pre-wrap fileset
+  (with-pass-thru fileset
     (let [tmpfiles (output-files fileset)
           tmpfiles-clj-mf (by-re [#".clj$" #".MF$"] tmpfiles)
           tmpfiles-others (not-by-re [#".clj$" #".MF$"] tmpfiles)]
@@ -52,26 +51,23 @@
       (is (empty? tmpfiles-others) "Output files (no .clj and .MD) should be empty"))
     fileset))
 
-(deftask sift-include-tests []
-  (boot-test/test-task
-   (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
-         (sift :include #{#".clj$" #".MD$"})
-         (include-tests))))
+(deftesttask sift-include-tests []
+  (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
+        (sift :include #{#".clj$" #".MD$"})
+        (include-tests)))
 
 (deftask ^:private include-invert-tests []
-  (with-pre-wrap fileset
+  (with-pass-thru fileset
     (let [tmpfiles (output-files fileset)
           tmpfiles-clj-mf (by-re [#".clj$" #".MF$"] tmpfiles)
           tmpfiles-others (not-by-re [#".clj$" #".MF$"] tmpfiles)]
       (is (empty? tmpfiles-clj-mf) ".clj and .MD files should not be in output")
-      (is (not (empty? tmpfiles-others)) "Output files (no .clj and .MD) should not be empty"))
-    fileset))
+      (is (not (empty? tmpfiles-others)) "Output files (no .clj and .MD) should not be empty"))))
 
-(deftask sift-include-invert-tests []
-  (boot-test/test-task
-   (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
-         (sift :include #{#"\.clj$" #"\.MF$"} :invert true)
-         (include-invert-tests))))
+(deftesttask sift-include-invert-tests []
+  (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
+        (sift :include #{#"\.clj$" #"\.MF$"} :invert true)
+        (include-invert-tests)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; sift --to-asset  ;;;
@@ -80,62 +76,54 @@
 ;; AR - adding this in order to test sift-mv
 
 (deftask ^:private to-asset-tests []
-  (with-pre-wrap fileset
+  (with-pass-thru fileset
     (let [mf-input-files (by-re [#".*\.MF$"] (input-files fileset))
           mf-output-files (by-re [#".*\.MF$"] (output-files fileset))]
       (is (empty? mf-input-files) "The .MF files should not have input role")
-      (is (not (empty? mf-output-files)) "The .MF files should have output role"))
-    fileset))
+      (is (not (empty? mf-output-files)) "The .MF files should have output role"))))
 
-(deftask sift-to-asset-tests []
-  (boot-test/test-task
-   (comp (sift :add-jar {'org.clojure/tools.reader #".*"} ;; populate
-               :to-asset #{#".*\.MF$"})
-         (to-asset-tests))))
+(deftesttask sift-to-asset-tests []
+  (comp (sift :add-jar {'org.clojure/tools.reader #".*"} ;; populate
+              :to-asset #{#".*\.MF$"})
+        (to-asset-tests)))
 
 (deftask ^:private to-asset-invert-tests []
-  (with-pre-wrap fileset
+  (with-pass-thru fileset
     (let [non-mf-input-files (not-by-re [#".*\.MF$"] (input-files fileset))
           non-mf-output-files (not-by-re [#".*\.MF$"] (output-files fileset))]
       (is (empty? non-mf-input-files) "The .MF files should not have input role")
-      (is (not (empty? non-mf-output-files)) "The .MF files should have output role"))
-    fileset))
+      (is (not (empty? non-mf-output-files)) "The .MF files should have output role"))))
 
-(deftask sift-to-asset-invert-tests []
-  (boot-test/test-task
-   (comp (sift :add-jar {'org.clojure/tools.reader #".*"} ;; populate
-               :to-asset #{#".*\.MF$"}
-               :invert true)
-         (to-asset-invert-tests))))
+(deftesttask sift-to-asset-invert-tests []
+  (comp (sift :add-jar {'org.clojure/tools.reader #".*"} ;; populate
+              :to-asset #{#".*\.MF$"}
+              :invert true)
+        (to-asset-invert-tests)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; sift --add-meta  ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
 (deftask ^:private add-meta-tests []
-  (with-pre-wrap fileset
+  (with-pass-thru fileset
     (let [tmpfiles (output-files fileset)
           tmpfiles-with-meta (filter :boot-test-tag tmpfiles)]
       (is (empty? (not-by-re [#".clj$"] tmpfiles-with-meta)) "non .clj files should not have :boot-test-tag metadata")
-      (is (seq (by-re [#".clj$"] tmpfiles-with-meta)) "only .clj files should have :boot-test-tag metadata"))
-    fileset))
+      (is (seq (by-re [#".clj$"] tmpfiles-with-meta)) "only .clj files should have :boot-test-tag metadata"))))
 
-(deftask sift-add-meta-tests []
-  (boot-test/test-task
-   (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
-         (sift :add-meta {#".clj$" :boot-test-tag})
-         (add-meta-tests))))
+(deftesttask sift-add-meta-tests []
+  (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
+        (sift :add-meta {#".clj$" :boot-test-tag})
+        (add-meta-tests)))
 
 (deftask ^:private add-meta-invert-tests []
-  (with-pre-wrap fileset
+  (with-pass-thru fileset
     (let [tmpfiles (output-files fileset)
           tmpfiles-with-meta (filter :boot-test-tag tmpfiles)]
       (is (empty? (by-re [#".clj$"] tmpfiles-with-meta)) ".clj files should not have :boot-test-tag metadata")
-      (is (seq (not-by-re [#".clj$"] tmpfiles-with-meta)) "only non .clj files should have :boot-test-tag metadata"))
-    fileset))
+      (is (seq (not-by-re [#".clj$"] tmpfiles-with-meta)) "only non .clj files should have :boot-test-tag metadata"))))
 
-(deftask sift-add-meta-invert-tests []
-  (boot-test/test-task
-   (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
-         (sift :add-meta {#".clj$" :boot-test-tag} :invert true)
-         (add-meta-invert-tests))))
+(deftesttask sift-add-meta-invert-tests []
+  (comp (sift :add-jar {'org.clojure/tools.reader #".*"}) ;; populate
+        (sift :add-meta {#".clj$" :boot-test-tag} :invert true)
+        (add-meta-invert-tests)))
