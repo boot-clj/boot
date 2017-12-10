6d04e77337fcc6e28e57651c6023484792d91712
Bozhidar Batsov <bozhidar.batsov@gmail.com>
2017-12-10 11:47:04 +0200

    Bump the nREPL dep to 0.2.13
    That's the current stable version of nREPL.

--- boot/pod/src/boot/repl.clj
+++ boot/pod/src/boot/repl.clj
@@ -8,7 +8,7 @@
     [boot.from.io.aviso.exception :refer [*fonts*]]))
 
 (def ^:dynamic *default-dependencies*
-  (atom '[[org.clojure/tools.nrepl "0.2.12" :exclusions [[org.clojure/clojure]]]]))
+  (atom '[[org.clojure/tools.nrepl "0.2.13" :exclusions [[org.clojure/clojure]]]]))
 
 (defn ^:private disable-exception-colors
   [handler]
