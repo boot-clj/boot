a8c066a2055bde4e0a9a0ee28d1ca3a6a9876097
Martin Klepsch <martinklepsch@googlemail.com>
2017-12-22 11:43:16 +0100

    remove githuburl var from App.java that is no longer used

--- boot/base/src/main/java/boot/App.java
+++ boot/base/src/main/java/boot/App.java
@@ -38,7 +38,6 @@ public class App {
     private static String                   appversion          = null;
     private static String                   channel             = "RELEASE";
     private static String                   booturl             = "http://boot-clj.com";
-    private static String                   githuburl           = "https://api.github.com/repos/boot-clj/boot/releases";
     private static boolean                  update_always       = false;
     private static ClojureRuntimeShim       aethershim          = null;
     private static HashMap<String, File[]>  depsCache           = null;
