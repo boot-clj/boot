b0b255e4fe04e53ccb7ba34bdeb4e068e4333cd8
xtreak <tir.karthi@gmail.com>
2018-07-05 15:55:50 +0000

    Bump reply version to 0.4.0

--- boot/worker/project.clj
+++ boot/worker/project.clj
@@ -23,7 +23,7 @@
                  ;; see https://github.com/boot-clj/boot/issues/82
                  [net.cgrand/parsley          "0.9.3" :exclusions [org.clojure/clojure]]
                  [mvxcvi/puget                "1.0.1"]
-                 [reply                       "0.3.7"]
+                 [reply                       "0.4.0"]
                  [cheshire                    "5.6.0"]
                  [clj-jgit                    "0.8.0"]
                  [clj-yaml                    "0.4.0"]
